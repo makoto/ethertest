b59cef7431d9efc5ce9a6650d1f8ad4109fb700f
Makoto <inouemak@googlemail.com>
2022-03-23 22:00:23 +0000

    Add CCIPReadEnabled flag

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const { ethers } = require("ethers");
+const { Resolver } = require('@ensdomains/ens-contracts') 
+const { hash } = require('@ensdomains/eth-ens-namehash')
+
 const url = `https://ropsten.infura.io/v3/${process.env.API_KEY}`
-console.log({url})
+// console.log({url})
 const provider = new ethers.providers.JsonRpcProvider(url);
+
+function getResolverContract(address, provider){
+  return new ethers.Contract(address, Resolver, provider)
+}
+
 async function main(){
     const block = await provider.getBlock()
-    console.log({block:block.number})
+        console.log({block:block.number})
     let resolver1 = await provider.getResolver('1.offchainexample.eth')
     console.log({block:block.number, offchainresolver:resolver1.address})
     let resolver2 = await provider.getResolver('makoto.hatch.eth')
@@ -13,21 +21,48 @@ async function main(){
     console.log({block:block.number, offchainaddress:address1})
     let address2 = await provider.resolveName('makoto.hatch.eth')
     console.log({block:block.number, makotohatchaddress:address2})
+    resolver1contract = getResolverContract(address1, provider) 
+    let addr11 = await resolver1contract['addr(bytes32)'](hash('1.offchainexample.eth'), {ccipReadEnabled:true})
+    console.log({addr11})
 }
 
 main()
 
 /* output
-~/.../tmp/ether $node index.js 
-{ block: 14418757 }
+
+node index.js 
 {
-  block: 14418757,
-  offchainresolver: '0xC1735677a60884ABbCF72295E88d47764BeDa282'
+  url: 'https://ropsten.infura.io/v3/2a2262de759044bf885ee7e2b0a8723f'
 }
-{ block: 14418757, makotohatchresolver: null }
+{ block: 12126983 }
 {
-  block: 14418757,
+  block: 12126983,
+  offchainresolver: '0x323401A92457Ac110d48aED5C8f020Ee3F8BF2D1'
+}
+{
+  block: 12126983,
+  makotohatchresolver: '0x8fc4C380c5d539aE631daF3Ca9182b40FB21D1ae'
+}
+{
+  block: 12126983,
   offchainaddress: '0xb8c2C29ee19D8307cb7255e1Cd9CbDE883A267d5'
 }
-{ block: 14418757, makotohatchaddress: null }
+{
+  block: 12126983,
+  makotohatchaddress: '0x0Bc0035cE036984749Fd22978394734B89690b5e'
+}
+(node:11453) UnhandledPromiseRejectionWarning: Error: call revert exception [ See: https://links.ethers.org/v5-errors-CALL_EXCEPTION ] (method="addr(bytes32)", errorArgs=null, errorName=null, errorSignature=null, reason=null, code=CALL_EXCEPTION, version=abi/5.6.0)
+    at Logger.makeError (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/logger/lib/index.js:233:21)
+    at Logger.throwError (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/logger/lib/index.js:242:20)
+    at Interface.decodeFunctionResult (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/contracts/node_modules/@ethersproject/abi/lib/interface.js:382:23)
+    at Contract.<anonymous> (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/contracts/lib/index.js:395:56)
+    at step (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/contracts/lib/index.js:48:23)
+    at Object.next (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/contracts/lib/index.js:29:53)
+    at fulfilled (/Users/makoto/work/ens/tmp/ether/node_modules/@ethersproject/contracts/lib/index.js:20:58)
+    at processTicksAndRejections (internal/process/task_queues.js:95:5)
+(Use `node --trace-warnings ...` to show where the warning was created)
+(node:11453) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
+(node:11453) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+
+
 */
