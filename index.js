f6b8814cfd1d3fede4cf4d03b28f7ca2a05d5ffa
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-03-28 11:48:28 +0100

    Error: missing revert data in call exception

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const { ethers } = require("ethers");
-const url = `https://mainnet.infura.io/v3/${process.env.API_KEY}`
+const IExtendedResolver = require('@ensdomains/offchain-resolver-contracts/artifacts/contracts/IExtendedResolver.sol/IExtendedResolver.json').abi
+const { Resolver } = require('@ensdomains/ens-contracts') 
+const { hash } = require('@ensdomains/eth-ens-namehash')
+
+const url = `https://ropsten.infura.io/v3/${process.env.API_KEY}`
+// console.log({url})
 const provider = new ethers.providers.JsonRpcProvider(url);
 
-async function main(){
-  let resolver = await provider.getResolver('1.offchainexample.eth')
-  let address = await provider.resolveName('1.offchainexample.eth')
-  let email = await resolver.getText('email')
-  console.log({resolver:resolver.address, address, email})
+function getResolverContract(address, provider){
+  return new ethers.Contract(address, [...IExtendedResolver,...Resolver], provider)
 }
+
+async function getRecord(name){
+  let resolver = await provider.getResolver(name)
+  let resolveName = await provider.resolveName(name)
+  let resolverContract = getResolverContract(resolver.address, provider)
+  console.log({hashname:hash(name), resolverContract})
+  let address = await resolverContract['addr(bytes32)'](hash(name), {ccipReadEnabled:true})
+  // let calldata = '0x3b3b57de1c9fb8c1fe76f464ccec6d2c003169598fdfcbcb6bbddf6af9c097a39fa0048c'
+  // let address = await resolverContract['resolve'](hash(name), calldata, {ccipReadEnabled:true})
+  // let address = await resolver.getAddress()
+  // let address0 = await resolver.getAddress(0)
+  // let email = await resolver.getText('email')
+  return({
+    name,
+    // resolverAddress:resolver.address,
+    resolveName,
+    address,
+    // address0,
+    // email
+  })
+}
+
+async function main(){
+    const block = await provider.getBlock()
+    console.log({block:block.number})
+    console.log(await getRecord('1.offchainexample.eth'))
+    // console.log(await getRecord('makoto.hatch.eth'))
+    // resolver2contract = getResolverContract(address2, provider) 
+    
+    // let addr22 = await resolver1contract['text'](hash('makoto.hatch.eth'), 'email', {ccipReadEnabled:true})
+    // // let addr11 = await resolver1contract['addr(bytes32)'](hash('makoto.hatch.eth'), {ccipReadEnabled:true})
+    // console.log({addr22})
+    // let addr11 = await resolver1contract['text'](hash('1.offchainexample.eth'), 'email', {ccipReadEnabled:true})
+    // console.log({addr11})
+}
+
 main()
 
 /* output
